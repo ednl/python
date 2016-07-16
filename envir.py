5bf22a64da951206cb9716f47e0da72470b1fc1a
Ewoud Dronkert <ednl@users.noreply.github.com>
2016-07-16 15:28:10 +0200

    Added motion

--- envir.py
+++ envir.py
@@ -1,13 +1,11 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
-from envirophat import light, weather, leds
+from envirophat import leds, light, weather, motion
 import time, datetime, os, sys
 
+leds.off()
 old = None
 while True:
-
-	# Flash LEDs as an activity indicator
-	leds.on()
 
 	# Hourly log files
 	now = datetime.datetime.now()
@@ -18,34 +16,42 @@ while True:
 		disk = os.statvfs("/home/pi")
 		freemb = disk.f_bsize * disk.f_bavail / 1024 / 1024
 		if freemb < 512:
-			print("envir.py: Low on disk space" + (", last log in " + old if old else "."))
-			sys.exit()
+			sys.exit("envir.py: Low on disk space" + (", last log in " + old if old else "."))
 
 		# Column headers on new log file
 		if not os.path.isfile(log):
 			with open(log, "w") as file:
-				file.write("Date,Time,CPU,Temp,Press,Light,Red,Green,Blue\n")
+				file.write("Date,Time,CPU,Temp,Press,Light,Red,Green,Blue,Heading,AccelX,AccelY,AccelZ\n")
 
 		old = log
 
-	# Read CPU temperature, round to tenths of degrees Celsius
+	# Read CPU temperature in degrees Celsius
 	with open("/sys/class/thermal/thermal_zone0/temp") as file:
-		cpu = round(float(file.read().strip()) / 100) / 10
+		cputemp = float(file.read().strip()) / 1000
+
+	# EnviroPhat measurements
+	temperature = weather.temperature()
+	pressure    = weather.pressure()
+	lightflux   = light.light()
+	r, g, b     = light.rgb()
+	heading     = motion.heading()
+	ax, ay, az  = motion.accelerometer()
 
 	# Comma Separated Values
-	csv = "{d:%F,%T},{c:.1f},{t:.1f},{p:.0f},{x},{},{},{}".format(
-		d = now,
-		c = cpu,
-		t = weather.temperature(),
-		p = weather.pressure(),
-		x = light.light(),
-		*light.rgb()
+	csv = "{n:%F,%T},{c:.1f},{t:.1f},{p:.0f},{f},{r},{g},{b},{h:.0f},{x:.1f},{y:.1f},{z:.1f}\n".format(
+		n = now,
+		c = cputemp,
+		t = temperature,
+		p = pressure,
+		f = lightflux,
+		r = r, g = g, b = b,
+		h = heading,
+		x = ax, y = ay, z = az
 	)
 
 	# Append to log file
 	with open(log, "a") as file:
-		file.write(csv + "\n")
+		file.write(csv)
 
-	# Wait 5 seconds = ~720 lines per log file
-	leds.off()
+	# Wait 5 seconds => ~720 lines per hourly log file = ~40 KB
 	time.sleep(5)
