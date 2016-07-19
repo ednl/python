140cdedf0e1104296bfc7d8e6062f75bf75cf5c5
Ewoud Dronkert <ednl@users.noreply.github.com>
2016-07-19 13:59:02 +0200

    Update, added left/right

--- radiocontrols.py
+++ radiocontrols.py
@@ -2,50 +2,84 @@
 
 from dothat import touch, lcd, backlight
 import subprocess as SP
-import signal, re
+import re, signal, time
 
 findnum = re.compile(r"\d+")
+station = 0
+stations = []
+with open("/home/pi/radio.txt") as file:
+	for line in file:
+		stations.append(line.split()[0])
 
-def clear():
-        lcd.clear()
-        backlight.off()
-        backlight.set_graph(0)
+def clear_all():
+	backlight.set_graph(0)
+	backlight.off()
+	lcd.clear()
 
-def status():
-        clear()
+def lcd_out(row, txt):
+	lcd.set_cursor_position(0, row)
+	lcd.write("{:16}".format(txt))
 
-        lcd.set_cursor_position(0, 0)
-        lcd.write("Stream: " + SP.check_output("station", universal_newlines=True).strip())
+def adj_vol():
+	volume = SP.check_output("mpc volume", shell=True, universal_newlines=True).strip().capitalize()
+	percent = float(findnum.search(volume).group()) / 100
 
-        lcd.set_cursor_position(0, 1)
-        vol = SP.check_output("mpc volume", shell=True, universal_newlines=True).strip()
-        lcd.write(vol.capitalize())
-        val = float(findnum.search(vol).group()) / 100
-        backlight.set_graph(val)
+	lcd_out(1, volume)
+	backlight.set_graph(percent)
 
-        lcd.set_cursor_position(0, 2)
-        lcd.write("Snooze: " + SP.check_output("snooze", universal_newlines=True).strip())
+	time.sleep(2)
+	backlight.set_graph(0)
 
-status()
+def full_status():
+	backlight.rgb(255, 255, 255)
+
+	stream = "Stream: " + SP.check_output("station", universal_newlines=True).strip()
+	lcd_out(0, stream)
+
+	snooze = "Snooze: " + SP.check_output("snooze", universal_newlines=True).strip()
+	lcd_out(2, snooze)
+
+	adj_vol()
+	backlight.off()
 
 @touch.on(touch.UP)
-def vol_up(channel, event):
-        SP.call("piradio vol +", shell=True)
-        status()
+def press_up(channel, event):
+	SP.call("piradio vol +", shell=True)
+	adj_vol()
 
 @touch.on(touch.DOWN)
-def vol_down(channel, event):
-        SP.call("piradio vol -", shell=True)
-        status()
+def press_down(channel, event):
+	SP.call("piradio vol -", shell=True)
+	adj_vol()
 
 @touch.on(touch.CANCEL)
-def radio_off(channel, event):
-        SP.call("piradio off", shell=True)
-        clear()
+def press_cancel(channel, event):
+	SP.call("piradio off", shell=True)
+	clear_all()
 
 @touch.on(touch.BUTTON)
-def radio_on(channel, event):
-        SP.call("piradio fip", shell=True)
-        status()
+def press_button(channel, event):
+	if 0 <= station < len(stations):
+		SP.call("piradio " + stations[station], shell=True)
+	full_status()
 
+@touch.on(touch.LEFT)
+def press_left(channel, event):
+	station -= 1
+	if station < 0:
+		station = len(stations) - 1
+	if 0 <= station < len(stations):
+		SP.call("piradio " + stations[station], shell=True)
+	full_status()
+
+@touch.on(touch.RIGHT)
+def press_right(channel, event):
+	station += 1
+	if station >= len(stations):
+		station = 0
+	if 0 <= station < len(stations):
+		SP.call("piradio " + stations[station], shell=True)
+	full_status()
+
+full_status()
 signal.pause()
