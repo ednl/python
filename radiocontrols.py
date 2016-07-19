39beb0769f52d7b79a85942e595057b2b4d54d51
Ewoud Dronkert <ednl@users.noreply.github.com>
2016-07-19 14:09:07 +0200

    Need keyword global to modify global variable. D'oh.

--- radiocontrols.py
+++ radiocontrols.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 
 from dothat import touch, lcd, backlight
-import subprocess as SP
-import re, signal, time
+import re, signal, time, subprocess
 
 findnum = re.compile(r"\d+")
 station = 0
@@ -21,64 +20,72 @@ def lcd_out(row, txt):
 	lcd.write("{:16}".format(txt))
 
 def adj_vol():
-	volume = SP.check_output("mpc volume", shell=True, universal_newlines=True).strip().capitalize()
+	volume = subprocess.check_output("mpc volume", shell=True, universal_newlines=True).strip().capitalize()
 	percent = float(findnum.search(volume).group()) / 100
 
 	lcd_out(1, volume)
 	backlight.set_graph(percent)
 
-	time.sleep(2)
+	time.sleep(1)
 	backlight.set_graph(0)
 
 def full_status():
 	backlight.rgb(255, 255, 255)
 
-	stream = "Stream: " + SP.check_output("station", universal_newlines=True).strip()
+	stream = "Stream: " + subprocess.check_output("station", universal_newlines=True).strip()
 	lcd_out(0, stream)
 
-	snooze = "Snooze: " + SP.check_output("snooze", universal_newlines=True).strip()
+	snooze = "Snooze: " + subprocess.check_output("snooze", universal_newlines=True).strip()
 	lcd_out(2, snooze)
 
 	adj_vol()
 	backlight.off()
 
+# Volume up
 @touch.on(touch.UP)
 def press_up(channel, event):
-	SP.call("piradio vol +", shell=True)
+	subprocess.call("piradio vol +", shell=True)
 	adj_vol()
 
+# Volume down
 @touch.on(touch.DOWN)
 def press_down(channel, event):
-	SP.call("piradio vol -", shell=True)
+	subprocess.call("piradio vol -", shell=True)
 	adj_vol()
 
+# Radio off
 @touch.on(touch.CANCEL)
 def press_cancel(channel, event):
-	SP.call("piradio off", shell=True)
+	subprocess.call("piradio off", shell=True)
 	clear_all()
 
+# Radio on
 @touch.on(touch.BUTTON)
 def press_button(channel, event):
 	if 0 <= station < len(stations):
-		SP.call("piradio " + stations[station], shell=True)
+		subprocess.call("piradio " + stations[station], shell=True)
 	full_status()
 
+# Previous station
 @touch.on(touch.LEFT)
 def press_left(channel, event):
+	global station
 	station -= 1
 	if station < 0:
 		station = len(stations) - 1
 	if 0 <= station < len(stations):
-		SP.call("piradio " + stations[station], shell=True)
+		subprocess.call("piradio " + stations[station], shell=True)
 	full_status()
 
+# Next station
 @touch.on(touch.RIGHT)
 def press_right(channel, event):
+	global station
 	station += 1
 	if station >= len(stations):
 		station = 0
 	if 0 <= station < len(stations):
-		SP.call("piradio " + stations[station], shell=True)
+		subprocess.call("piradio " + stations[station], shell=True)
 	full_status()
 
 full_status()
