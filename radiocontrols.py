c90709e834c11b858a09c15cee4b4dab4f206723
Ewoud Dronkert <ednl@users.noreply.github.com>
2016-07-20 13:40:40 +0200

    Use threading

--- radiocontrols.py
+++ radiocontrols.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 from dothat import touch, lcd, backlight
-import re, signal, time, subprocess
+import re, signal, time, subprocess, threading
 
 findnum = re.compile(r"\d+")
 index = 0
@@ -10,64 +10,68 @@ with open("/home/pi/radio.txt") as file:
 	for line in file:
 		stations.append(line.split()[0])
 
-def reset():
-	backlight.set_graph(0)
-	backlight.off()
-	lcd.clear()
-
 def line(row, txt):
 	lcd.set_cursor_position(0, row)
 	lcd.write("{:16}".format(txt))
 
-def show_volume():
+def clear_graph():
+	backlight.set_graph(0)
+
+def clear_all():
+	backlight.set_graph(0)
+	backlight.off()
+	lcd.clear()
+
+def show():
+	global index
+	backlight.rgb(255, 255, 255)
+	t1 = threading.Timer(4.0, backlight.off)
+	t1.start()
+
+	# LCD row 1: station name
+	station = subprocess.check_output("station", universal_newlines=True).strip()
+	line(0, "Stream: " + station)
+
+	try:
+		index = stations.index(station)
+	except ValueError:
+		index = 0
+
+	# LCD row 2: volume level
 	volume = subprocess.check_output("mpc volume", shell=True, universal_newlines=True).strip().capitalize()
 	line(1, volume)
 
 	percent = float(findnum.search(volume).group()) / 100
 	backlight.set_graph(percent)
+	t2 = threading.Timer(1.5, clear_graph)
+	t2.start()
 
-	time.sleep(1)
-	backlight.set_graph(0)
-
-def show_all():
-	global index
-	backlight.rgb(255, 255, 255)
-
-	station = subprocess.check_output("station", universal_newlines=True).strip()
-	try:
-		index = stations.index(station)
-	except ValueError:
-		index = 0
-	line(0, "Stream: " + station)
-
+	# LCD row 3: snooze time
 	snooze = subprocess.check_output("snooze", universal_newlines=True).strip()
 	line(2, "Snooze: " + snooze)
-
-	show_volume()
-	backlight.off()
 
 def tune():
 	if 0 <= index < len(stations):
 		subprocess.call("piradio " + stations[index], shell=True)
-	show_all()
+	show()
 
 # Volume up
 @touch.on(touch.UP)
 def press_up(channel, event):
 	subprocess.call("piradio vol +", shell=True)
-	show_volume()
+	show()
 
 # Volume down
 @touch.on(touch.DOWN)
 def press_down(channel, event):
 	subprocess.call("piradio vol -", shell=True)
-	show_volume()
+	show()
 
 # Radio off
 @touch.on(touch.CANCEL)
 def press_cancel(channel, event):
 	subprocess.call("piradio off", shell=True)
-	reset()
+	clear_all()
 
 # Radio on
 @touch.on(touch.BUTTON)
@@ -92,5 +96,5 @@ def press_right(channel, event):
 		index = 0
 	tune()
 
-show_all()
+show()
 signal.pause()
