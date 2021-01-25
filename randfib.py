10e41ba0b9f47285f47ba29ed6e26b288874b1f2
Ewoud Dronkert <ewoud@dronkert.net>
2021-01-25 15:27:06 +0100

    variable names

--- randfib.py
+++ randfib.py
@@ -6,25 +6,25 @@ import math
 import random
 random.seed()
 
-v = 1.131_988_248_794_3
+viswanath = 1.131_988_248_794_3
 
-i = j = 0
-p = q = m = 1
-while (i < 1_000_000):
-    t = p
-    if random.randint(0, 1) == 0:
+n = decade = 0
+p = q = mod = 1
+while (n < 1_000_000):
+    tmp = p
+    if random.randint(0, 1):
         p += q
     else:
         p -= q
-    q = t
-    i += 1
-    if i % m == 0:
-        if p != 0:
-            r = math.exp(math.log(abs(p)) / i)
-            print(i, r, abs(r - v))
+    q = tmp
+    n += 1
+    if n % mod == 0:
+        if p:
+            root = math.exp(math.log(abs(p)) / n)
+            print(n, root, abs(root - viswanath))
         else:
-            print(i, p)
-        j += 1
-        if j == 9:
-            m *= 10
-            j = 0
+            print(n, p)
+        decade += 1
+        if decade == 9:
+            mod *= 10
+            decade = 0
