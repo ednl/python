c9118dba66f06009a005e8366924606d4794ec43
E. Dronkert <ednl@users.noreply.github.com>
2022-10-03 14:59:15 +0200

    More clarification in comments

--- sintable.py
+++ sintable.py
@@ -1,12 +1,14 @@
-# Faster sin/cos function by lookup table
+# Faster sin/cos function by using a lookup table
 # Example code for Daniel Shiffman made by Ewoud Dronkert
 # https://twitter.com/ednl
 # https://github.com/ednl
 
 from math import sin, cos, tau
 
-# Quadrants and circle below are not in degrees but in "index units" defined by the step
-step = 3                # steps taken in degrees, must divide 90
+# Quadrants and circle below are not in degrees but in "index units" as defined by the step
+# If every integer degree is needed, then step = 1 and most of the code can be simplified
+# If fractional degrees are also needed, then this code needs to be modified!
+step = 3                # step in degrees, must divide 90
 quad1 = 90 // step      # first quadrant 90 degrees [integer]
 quad2 = 180 // step     # second quadrant 180 degrees [integer]
 quad3 = 270 // step     # third quadrant 270 degrees [integer]
@@ -23,18 +25,22 @@ def index2deg(a):
 def index2rad(a):
     return a * i2r
 
-# Make the lookup table indexed 0..quad1 inclusive (0..90 degrees inclusive)
-# In real code, this should be a list of literal values
-# Use the minimum amount of decimals necessary, here: 5
+# Generate the lookup table with index 0..quad1 inclusive (= 0..90 degrees inclusive)
+# In real code, this could (should) be a list of literal values
+# Use the minimum amount of decimals necessary, choice here: 5
 sintable = [round(sin(index2rad(i)), 5) for i in range(quad1 + 1)]
 
-# Give sine of argument in "index units" from lookup table
+# Give sine of argument a given in "index units" by using lookup table
 # Argument 'a' is in stepped degrees where in this example the step is 3, so:
 #   a = -1 => -3 degrees
 #   a =  0 =>  0 degrees
 #   a =  1 =>  3 degrees
 #   a = 30 => 90 degrees
 #   a = 31 => 93 degrees
+# Argument a can be any integer but needs to be normalised, so this is slow for very big
+# (positive or negative) values. It depends on the CPU and language implementation
+# whether "mod" is faster than repeated adding/subtracting. On modern architectures,
+# mod is definitely faster, but on the Apple IIe? I have no idea :)
 def sinlookup(a):
     # Normalise a to the range 0..circle inclusive
     while a < 0:
@@ -52,7 +58,7 @@ def sinlookup(a):
         return sintable[quad2 - a]
     return sintable[a]
 
-# Same but for cosine
+# Same for cosine
 def coslookup(a):
     while a < 0:
         a += circle
@@ -67,7 +73,7 @@ def coslookup(a):
     return sintable[quad1 - a]
 
 # Test for a wide range of "index units"
-# Display difference between lookup value and calculation
+# and display difference between lookup value and calculation
 for i in range(-quad1, circle + quad1 + 1, 10):
     s1 = sinlookup(i)
     s2 = round(sin(index2rad(i)), 5)
